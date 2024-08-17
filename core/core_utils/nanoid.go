f4597fcc8a8520389ea3e7fd4715b03ee3172dc4
junyu <rainrcn@qq.com>
2024-08-17 14:52:13 +0800

    ..

--- core/core_utils/nanoid.go
+++ core/core_utils/nanoid.go
@@ -3,6 +3,7 @@ package core_utils
 import gonanoid "github.com/matoous/go-nanoid/v2"
 
 const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const alphabet_apparent = "23456789ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz"
 
 func NanoId() (string, error) {
 	return gonanoid.Generate(alphabet, 28)
